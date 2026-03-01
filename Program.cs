88df2ccda9c538b03eb9f17d1192e6d78f148c9f
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-01 13:22:33 +0530

    feat: add basic Subtraction method for calculator functionality

--- Program.cs
+++ Program.cs
@@ -11,12 +11,19 @@
             Console.WriteLine("Enter second number: ");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Addition: " + Add(firstNumber, secondNumber));
+            // Console.WriteLine("Addition: " + Add(firstNumber, secondNumber));
+             Console.WriteLine("Subtraction: " + Subtract(firstNumber, secondNumber));
         }
 
         static int Add(int a, int b)
         {
             return a + b;
         }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
     }
 }
