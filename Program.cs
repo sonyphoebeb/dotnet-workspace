7c0d4e556cb03cf9aa4221588b814bdbb8e44988
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-01 13:16:02 +0530

    feat: add basic Add method for integer addition

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SonyConsoleApplication
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + Add(firstNumber, secondNumber));
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
